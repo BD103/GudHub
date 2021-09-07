ffba64f490216bf195cfbe6983dbaad1dab8e31a
BD103 <>
2021-09-07 15:47:24 +0000

    feat: add repo view and more

--- app.js
+++ app.js
@@ -6,9 +6,23 @@ const port = 8000;
 app.set("view engine", "pug");
 app.set("views", "./views");
 
+app.use(express.static("public"));
+
+// Index view
 app.get("/", async (_, res) => {
   console.log("[DEBUG] Index");
   res.render("index");
+});
+
+// Repo view
+app.get("/:user/:repo", function (req, res) {
+  console.log("[DEBUG] Repo: " + req.params.user + "/" + req.params.repo);
+  res.render("repo", {
+    user: req.params.user,
+    repo: req.params.repo,
+    folders: [".config", ".github", "views"],
+    files: [".gitignore", ".replit", "LICENSE", "README.md", "app.js", "package-lock.json", "package.json", "replix.nix"]
+  });
 });
 
 app.listen(port, () => {
